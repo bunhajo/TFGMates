246b004d5f3498599667aff55a399cdf8baf3ebe
Juan Tornero <juan@metriplica.com>
2017-04-07 14:05:50 +0200

    commit 07/04/2017

--- GHMM-ANN.R
+++ GHMM-ANN.R
@@ -26,106 +26,104 @@ BaumWelch_ANN = function(returns, w, p, n_states=2, Tolerance=7*10^{-2}){
     
     
     for(i in 1:n_states){
-        B[,i] = exp(-.5*apply(as.matrix(w)*as.matrix(retornos), 1, function(x)sum(x)))/((2*pi)^(k/2))
+        B[,i] = exp(t(w[,i]%*%t(returns)))
     }
-    
-    # % for t=1:T
-    # %     B(t,1) =  exp(-.5*((returns(t)-w(1))/sigma(1)).^2)/(sqrt(2*pi)*sigma(1));
-    # %     B(t,2) =  exp(-.5*((returns(t)-w(2))/sigma(2)).^2)/(sqrt(2*pi)*sigma(2));
-    # % end
-    # 
-    
-    
-    # SSECCION B
-    
-    forward[1,]=p*B[1,]
-    forward[1,] = forward[1,]/sum(forward[1,])
-    
-    for (t in 2:L){
-      aux=c(rep(0,n_states))
-      for(i in 1:n_states){
-        aux[i] =  aux[i] + sum(forward[t-1,]*A[,i])
-      }
-      forward[t,]=aux*B[t,]
-      forward[t,] = forward[t,]/sum(forward[t,])
-    }
-    
-    #SECCION C
-    
-    backward[L,]=B[L,]
-    backward[L,]=backward[L,]/sum(backward[L,])
-    
-    t=L-1
-    while (t>=1){
-      aux=c(rep(0,n_states))
-      for(i in 1:n_states){
-        aux[i] =  aux[i] + sum(A[,i]*backward[t+1,])
-      }
-      backward[t,]=aux*B[t+1,]
-      backward[t,]=backward[t,]/sum(backward[t,])
-      t=t-1
-    }               
-    
-    #SECCION D
-    
-    
-    for (t in 1:L){
-      smoothed[t,]= forward[t,]*backward[t,]
-      smoothed[t,]= smoothed[t,]/sum(smoothed[t,])
-    }
-    
-    t=1
-    while(t<=L-1){
-      aux = diag(0,n_states,n_states)
-      
-      for(j in 1:n_states){
-        aux[j,]=as.matrix(forward[t,j]*backward[t+1,]*B[t+1,])
-      } 
-      
-      xi[[t]] = A*aux
-      xi[[t]]=xi[[t]]/sum(xi[[t]])
-      t=t+1
+    for(i in 1:L){
+      R[i,]=B[i,]/sum(B[i,])
     }
     
     #SECCION E
     
+    p[1]=sum(R[,1])/length(R[,1])
+    p[2]=sum(R[,2])/length(R[,2])
+    # 
+    # exp_num_transitions=data.frame(c(rep(0,n_states)),c(rep(0,n_states)))
+    # for(i in 1:n_states){
+    #   for(j in 1:n_states){
+    #     exp_num_transitions[i,j]=sum(sapply(lapply(xi, function(x)sum(x[i,j])), function(x)(sum(x))))  
+    #   }
+    # }
     
-    p=smoothed[1,]
-    
-    exp_num_transitions=data.frame(c(rep(0,n_states)),c(rep(0,n_states)))
     for(i in 1:n_states){
-      for(j in 1:n_states){
-        exp_num_transitions[i,j]=sum(sapply(lapply(xi, function(x)sum(x[i,j])), function(x)(sum(x))))  
-      }
-    }
-    
-    for(i in 1:n_states){
-      if(sum(sapply(lapply(xi, function(x)sum(x[i,])), function(x)(sum(x))))!=0){
-        A[i,] = exp_num_transitions[i,]/sum(sapply(lapply(xi, function(x)sum(x[i,])), function(x)(sum(x))))
-      }else{A[i,]=c(rep(0,n_states))}
       if(k!=1){
         for(j in 1:k){
           #Mirar
-          if(sum(smoothed[,i])!=0){w[j,i]=sum(smoothed[,i]*returns[,j])/sum(smoothed[,i])}else{w[i,j]=0}
+          w[j,i]=sum(R[,i]*returns[,j])/sum(R[,i])
           
           #sigma[i,]=sqrt(sum(smoothed[,i]*(returns-as.data.frame(w)[i,])^2)/sum(smoothed[,i]))
           #sigma[i,]=sqrt(apply((smoothed[,i]%*%(returns-as.data.frame(w)[i,])^2),1, function(x) sum(x))/sum(smoothed[,i]))
         }
       }else{
-        w[i,]=sum(smoothed[,i]*returns)/sum(smoothed[,i])
-       # sigma[i,]=apply((smoothed[,i]*(returns-w[i,])^2),2, function(x)sum(x)/sum(smoothed[,i]))
+        w[i,]=sum(B[,i]*returns)/sum(B[,i])
+        # sigma[i,]=apply((smoothed[,i]*(returns-w[i,])^2),2, function(x)sum(x)/sum(smoothed[,i]))
       }
-      
-      
-      
       #TOLERANCE
-      likelihood[iteration, i] = log(sum(smoothed[,i]/sum(smoothed[,i])*exp(-.5*apply(as.matrix(w)*as.matrix(retornos), 1, function(x)sum(x)))/((2*pi)^(k/2))))
-
+      # likelihood[iteration, i] = log(sum(smoothed[,i]/sum(smoothed[,i])*exp(t(w[,i]%*%t(returns)))))
+      
     }
     if(iteration>1){change_likelihood= abs(likelihood[iteration,]-likelihood[iteration-1,])}
     iteration=iteration+1
   }
   
-  return(list(w=w, p = p, A=A, forward=forward, backward=backward, smoothed=smoothed, esp_num_transitions=exp_num_transitions))
+  return(list(w=w, p = p, R=R))
   
 }
+     
+     # for t=1:T
+     #     B(t,1) =  exp(-.5*((returns(t)-w(1))/sigma(1)).^2)/(sqrt(2*pi)*sigma(1));
+     #     B(t,2) =  exp(-.5*((returns(t)-w(2))/sigma(2)).^2)/(sqrt(2*pi)*sigma(2));
+     # end
+
+    
+    
+    # SSECCION B
+    
+    # forward[1,]=p*B[1,]
+    # forward[1,] = forward[1,]/sum(forward[1,])
+    # 
+    # for (t in 2:L){
+    #   aux=c(rep(0,n_states))
+    #   for(i in 1:n_states){
+    #     aux[i] =  aux[i] + sum(forward[t-1,]*A[,i])
+    #   }
+    #   forward[t,]=aux*B[t,]
+    #   forward[t,] = forward[t,]/sum(forward[t,])
+    # }
+    # 
+    # #SECCION C
+    # 
+    # backward[L,]=B[L,]
+    # backward[L,]=backward[L,]/sum(backward[L,])
+    # 
+    # t=L-1
+    # while (t>=1){
+    #   aux=c(rep(0,n_states))
+    #   for(i in 1:n_states){
+    #     aux[i] =  aux[i] + sum(A[,i]*backward[t+1,])
+    #   }
+    #   backward[t,]=aux*B[t+1,]
+    #   backward[t,]=backward[t,]/sum(backward[t,])
+    #   t=t-1
+    # }
+    # 
+    # #SECCION D
+    # 
+    # 
+    # for (t in 1:L){
+    #   smoothed[t,]= forward[t,]*backward[t,]
+    #   smoothed[t,]= smoothed[t,]/sum(smoothed[t,])
+    # }
+    # 
+    # t=1
+    # while(t<=L-1){
+    #   aux = diag(0,n_states,n_states)
+    # 
+    #   for(j in 1:n_states){
+    #     aux[j,]=as.matrix(forward[t,j]*backward[t+1,]*B[t+1,])
+    #   }
+    # 
+    #   xi[[t]] = A*aux
+    #   xi[[t]]=xi[[t]]/sum(xi[[t]])
+    #   t=t+1
+    # }
+
--- impact_recognition.R
+++ impact_recognition.R
@@ -4,9 +4,8 @@ library(dplyr)
 library(neuralnet)
 
 
-####  HMM
 source(paste(getwd(), "/","Baum-Welch.R", sep=""))
-
+source(paste(getwd(), "/","GHMM-ANN.R", sep=""))
 
 df  = read.delim("DesigualB2C_ANN.txt")
 
@@ -17,109 +16,13 @@ for(i in 1:(nrow(df_retornos))){
   }
 }
 
-retornos_sess=df_retornos[,2]
-retornos_users=df_retornos[,3]
-retornos_transactions=df_retornos[,6]
-retornos_newusers=df_retornos[,5]
-retornos_bounces=df_retornos[,4]
-
-var_sess=BaumWelch(returns = retornos_sess, #mu=c(median(retornos_sess[which(retornos_sess>0)]),mean(retornos_sess[which(retornos_sess>0)]), mean(retornos_sess[which(retornos_sess<0)]), median(retornos_sess[which(retornos_sess<0)])),
-                   mu=c(-0.2,0.2),
-                   sigma= rep(var(retornos_sess),2), n_states = 2)
-var_users=BaumWelch(returns = retornos_users, #mu=c(median(retornos_users[which(retornos_users>0)]),0,median(retornos_users[which(retornos_users<0)])), 
-                    mu=c(-0.4,-0.2,0.2,0.3),                   
-                    sigma= rep(var(retornos_users),4), n_states = 4)
-var_transactions=BaumWelch(returns = retornos_transactions, #mu=c(median(retornos_transactions[which(retornos_transactions>0)]),0,median(retornos_transactions[which(retornos_transactions<0)])),
-                           mu=c(-0.4,-0.2,0.2,0.3),
-                           sigma= rep(var(retornos_transactions),4), n_states = 4)
-var_newusers=BaumWelch(returns = retornos_newusers, #mu=c(median(retornos_newusers[which(retornos_newusers>0)]),0,median(retornos_newusers[which(retornos_newusers<0)])),
-                       mu=c(-0.4,-0.2,0.2,0.3),
-                       sigma= rep(var(retornos_newusers),4), n_states = 4)
-var_bounces=BaumWelch(returns = retornos_bounces, #mu=c(median(retornos_bounces[which(retornos_bounces>0)]),0,median(retornos_bounces[which(retornos_bounces<0)])),
-                      mu=c(-0.4,-0.2,0.2,0.3),
-                      sigma= rep(var(retornos_bounces),4), n_states = 4)
-
-smoothed_users = var_users$smoothed
-names(smoothed_users)=c("1","2","3","4")
-A=double()
-for (i in 1:nrow(smoothed_users)) {
-  A[i]=as.double(names(smoothed_users)[which(t(smoothed_users[i,])==max(t(smoothed_users[i,])))])
-}
-
-smoothed_sessions= var_sess$smoothed
-names(smoothed_sessions)=c("1","2")
-B=double()
-for (i in 1:nrow(smoothed_sessions)) {
-  B[i]=as.double(names(smoothed_sessions)[which(t(smoothed_sessions[i,])==max(t(smoothed_sessions[i,])))])
-}
-
-
-smoothed_transactions= var_transactions$smoothed
-names(smoothed_transactions)=c("1","2","3","4")
-C=double()
-for (i in 1:nrow(smoothed_transactions)) {
-  C[i]=as.double(names(smoothed_transactions)[which(t(smoothed_transactions[i,])==max(t(smoothed_transactions[i,])))])
-}
-
-
-smoothed_newusers= var_newusers$smoothed
-names(smoothed_newusers)=c("1","2","3","4")
-D=double()
-for (i in 1:nrow(smoothed_newusers)) {
-  D[i]=as.double(names(smoothed_newusers)[which(t(smoothed_newusers[i,])==max(t(smoothed_newusers[i,])))])
-}
-
-
-smoothed_bounces= var_bounces$smoothed
-names(smoothed_bounces)=c("1","2","3", "4")
-E=double()
-for (i in 1:nrow(smoothed_bounces)) {
-  E[i]=as.double(names(smoothed_bounces)[which(t(smoothed_bounces[i,])==max(t(smoothed_bounces[i,])))])
-}
-
-
-df_retornos$smoothed=as.double(E)
-df$smoothed=as.double(c(2,E))
-ggplot(df_retornos, aes(x=date, y=sessions, group=1, colour= factor(round(smoothed,0))))+geom_line(size=0.2) + geom_smooth()+theme(axis.text.x = element_text(angle=30))+
-  scale_x_discrete(limits=df$date,breaks=df$date[seq(1,366,10)])+scale_colour_discrete(h=c(0,180), l=60, c=180)
-
-
-df_retornos$smoothed=as.double(A)
-df$smoothed=as.double(c(2,A))
-p=ggplot(df, aes(x=date, y=sessions, group=1, colour= factor(smoothed)))+geom_line(size=0.2) + geom_smooth()+theme(axis.text.x = element_text(angle=30))+
-  scale_x_discrete(limits=df$date,breaks=df$date[seq(1,400,10)])+scale_colour_manual(labels=c("1","2","3"), values=c("green", "yellow", "red"))
-
-
-
-df_retornos$smoothed=as.double(B)
-df$smoothed=as.double(c(2,B))
-p=ggplot(df_retornos, aes(x=date, y=sessions, group=1, colour= factor(smoothed)))+geom_point(size=3)+geom_line(color="black") + geom_smooth()+theme(axis.text.x = element_text(angle=30))+
-  scale_x_discrete(limits=df$date,breaks=df$date[seq(1,366,10)])+scale_colour_discrete(h=c(0,180), l=50)
-q=ggplot(df[-1,], aes(x=date, y=sessions, group=1, colour= factor(smoothed)))+geom_point(size=3)+geom_line(color="black") + geom_smooth()+theme(axis.text.x = element_text(angle=30))+
-  scale_x_discrete(limits=df$date,breaks=df$date[seq(1,366,10)])+scale_colour_discrete(h=c(0,180), l=50)
-
-multiplot(p,q)
-
-
-ggplot(data=df_retornos, aes(x=sessions))+
-  geom_density(alpha=.2, fill="blue")+
-  stat_function(fun=dnorm, args = list(mean=var_sess$mu[1,], sd=sqrt(var_sess$sigma[1,])), colour="green")+
-  stat_function(fun=dnorm, args = list(mean=var_sess$mu[2,], sd=sqrt(var_sess$sigma[2,])), colour="red")+
-  stat_function(fun=dnorm, args = list(mean=var_sess$mu[4,], sd=sqrt(var_sess$sigma[3,])), colour="orange")+
-  stat_function(fun=dnorm, args = list(mean=var_sess$mu[3,], sd=sqrt(var_sess$sigma[4,])), colour="yellow")+
-  scale_x_continuous(breaks = round(seq(min(df_retornos$sessions), max(df_retornos$sessions), by = 0.1),1)) 
-geom_density(aes(x=users), fill="green", alpha=0.2)+
-  geom_density(aes(x=bounces), fill="red", alpha=.2)+
-  geom_density(aes(x=newusers), fill="yellow",alpha=0.2)+
-  
-  
 #### ANN
   
   df_retornos$smoothed=0
-  df_retornos$smoothed[which(df_retornos$sessions>0.1)]=1
+  df_retornos$smoothed[which(df_retornos$sessions>0)]=1
   St_4 = df_retornos
   df_retornos$smoothed=0
-  df_retornos$smoothed[which(df_retornos$sessions>0 & df_retornos$sessions<0.1)]=1
+  df_retornos$smoothed[which(df_retornos$sessions<0)]=1
   St_3 = df_retornos
   df_retornos$smoothed=0
   df_retornos$smoothed[which(df_retornos$sessions<0 & df_retornos$sessions>-0.1)]=1
@@ -134,29 +37,155 @@ geom_density(aes(x=users), fill="green", alpha=0.2)+
   net_st_2=neuralnet(smoothed ~ sessions + users + bounceRate + transactions, St_2, 
                      hidden = 4, threshold = 0.001, stepmax = 100000)
   net_st_3=neuralnet(smoothed ~ sessions + users + bounceRate + transactions, St_3, 
-                     hidden = 4, threshold = 0.001, stepmax = 100000)
+                     hidden = 1, threshold = 0.001, stepmax = 100000)
   net_st_4=neuralnet(smoothed ~ sessions + users + bounceRate + transactions, St_4, 
-                     hidden = 4, threshold = 0.001, stepmax = 100000)
+                     hidden = 1, threshold = 0.001, stepmax = 100000)
   
-  A=(compute(net_st_1, df_retornos[,c(2,3,4,6)])$net.result-1)^2
-  B=(compute(net_st_2, df_retornos[,c(2,3,4,6)])$net.result-1)^2
-  C=(compute(net_st_3, df_retornos[,c(2,3,4,6)])$net.result-1)^2
-  D=(compute(net_st_4, df_retornos[,c(2,3,4,6)])$net.result-1)^2
+  w=data.frame(unlist(net_st_3$weights)[2:5] , unlist(net_st_4$weights)[2:5])
+  netp=data.frame(unlist(net_st_3$weights)[1] , unlist(net_st_4$weights)[1])
   
-  error=data.frame(A,B,C,D)
-  states= df_retornos$smoothed
-  names(states)=c("1","2","3", "4")
-  E=double()
-  for (i in 1:nrow(error)) {
-    E[i]=as.double(names(states)[which(t(error[i,])==min(t(error[i,])))])
+  results=BaumWelch_ANN(returns = df_retornos[,c(2,3,4,6)], w=w,p=p,n_states = 2)
+  
+  net_st_4$weights[[1]][[1]]=data.frame(c(p[2], w[,2] ))
+  net_st_3$weights[[1]][[1]]=data.frame(c(p[1], w[,1] ))
+  
+  compute_w <-function (x, covariate, rep = 1) 
+  {
+    nn <- x
+    linear.output <- nn$linear.output
+    weights <- nn$weights[[rep]]
+    length.weights <- length(weights)
+    covariate <- as.matrix(cbind(1, covariate))
+    act.fct <- function(x){x}
+    neurons <- list(covariate)
+    if (length.weights > 1) 
+      for (i in 1:(length.weights - 1)) {
+        temp <- neurons[[i]] %*% weights[[i]]
+        act.temp <- act.fct(temp)
+        neurons[[i + 1]] <- cbind(1, act.temp)
+      }
+    temp <- neurons[[length.weights]] %*% weights[[length.weights]]
+    if (linear.output) 
+      net.result <- temp
+    else net.result <- act.fct(temp)
+    list(neurons = neurons, net.result = net.result)
   }
   
-  df_retornos$smoothed=as.double(B)
-  df$smoothed=as.double(c(2,B))
-  ggplot(df_retornos, aes(x=date, y=sessions, group=1, colour= factor(smoothed)))+geom_point(size=3)+geom_line(color="black") + geom_smooth()+theme(axis.text.x = element_text(angle=30))+
+  C=compute_w(net_st_3, df_retornos[,c(2,3,4,6)])$net.result
+  D=compute_w(net_st_4, df_retornos[,c(2,3,4,6)])$net.result
+  
+  names(smoothed)=c("1","2")
+  D=double()
+  for (i in 1:nrow(smoothed)) {
+    D[i]=as.double(names(smoothed)[which(t(smoothed[i,])==max(t(smoothed[i,])))])
+  }
+  
+  df_retornos$smoothed=B
+  df$smoothed=c(1,B)
+  ggplot(df, aes(x=date, y=sessions, group=1, colour= factor(smoothed)))+geom_point(size=3)+geom_line(color="black") + geom_smooth()+theme(axis.text.x = element_text(angle=30))+
     scale_x_discrete(limits=df$date,breaks=df$date[seq(1,366,10)])+scale_colour_manual(labels=c("1","2","3"), values=c("red", "green", "yellow"))
   
-  ggplot(df, aes(x=date, y=sessions, group=1, colour= factor(smoothed)))+geom_line(size=0.2) + geom_smooth()+theme(axis.text.x = element_text(angle=30))+
+  
+  ggplot(df_retornos, aes(x=date, y=sessions, group=1, colour= factor(smoothed)))+geom_line(size=0.2) + geom_smooth()+theme(axis.text.x = element_text(angle=30))+
 scale_x_discrete(limits=df$date,breaks=df$date[seq(1,366,10)])+scale_colour_manual(labels=c("1","2","3"), values=c("red", "green", "yellow"))
+  
+  
+  
+  
+  # 
+  # retornos_sess=df_retornos[,2]
+  # retornos_users=df_retornos[,3]
+  # retornos_transactions=df_retornos[,6]
+  # retornos_newusers=df_retornos[,5]
+  # retornos_bounces=df_retornos[,4]
+  # 
+  # var_sess=BaumWelch(returns = retornos_sess, #mu=c(median(retornos_sess[which(retornos_sess>0)]),mean(retornos_sess[which(retornos_sess>0)]), mean(retornos_sess[which(retornos_sess<0)]), median(retornos_sess[which(retornos_sess<0)])),
+  #                    mu=c(-0.2,0.2),
+  #                    sigma= rep(var(retornos_sess),2), n_states = 2)
+  # var_users=BaumWelch(returns = retornos_users, #mu=c(median(retornos_users[which(retornos_users>0)]),0,median(retornos_users[which(retornos_users<0)])), 
+  #                     mu=c(-0.4,-0.2,0.2,0.3),                   
+  #                     sigma= rep(var(retornos_users),4), n_states = 4)
+  # var_transactions=BaumWelch(returns = retornos_transactions, #mu=c(median(retornos_transactions[which(retornos_transactions>0)]),0,median(retornos_transactions[which(retornos_transactions<0)])),
+  #                            mu=c(-0.4,-0.2,0.2,0.3),
+  #                            sigma= rep(var(retornos_transactions),4), n_states = 4)
+  # var_newusers=BaumWelch(returns = retornos_newusers, #mu=c(median(retornos_newusers[which(retornos_newusers>0)]),0,median(retornos_newusers[which(retornos_newusers<0)])),
+  #                        mu=c(-0.4,-0.2,0.2,0.3),
+  #                        sigma= rep(var(retornos_newusers),4), n_states = 4)
+  # var_bounces=BaumWelch(returns = retornos_bounces, #mu=c(median(retornos_bounces[which(retornos_bounces>0)]),0,median(retornos_bounces[which(retornos_bounces<0)])),
+  #                       mu=c(-0.4,-0.2,0.2,0.3),
+  #                       sigma= rep(var(retornos_bounces),4), n_states = 4)
+  # 
+  # smoothed_users = var_users$smoothed
+  # names(smoothed_users)=c("1","2","3","4")
+  # A=double()
+  # for (i in 1:nrow(smoothed_users)) {
+  #   A[i]=as.double(names(smoothed_users)[which(t(smoothed_users[i,])==max(t(smoothed_users[i,])))])
+  # }
+  # 
+  # smoothed_sessions= smoothed
+  # names(smoothed_sessions)=c("1","2")
+  # B=double()
+  # for (i in 1:nrow(smoothed_sessions)) {
+  #   B[i]=as.double(names(smoothed_sessions)[which(t(smoothed_sessions[i,])==max(t(smoothed_sessions[i,])))])
+  # }
+  # 
+  # 
+  # smoothed_transactions= var_transactions$smoothed
+  # names(smoothed_transactions)=c("1","2","3","4")
+  # C=double()
+  # for (i in 1:nrow(smoothed_transactions)) {
+  #   C[i]=as.double(names(smoothed_transactions)[which(t(smoothed_transactions[i,])==max(t(smoothed_transactions[i,])))])
+  # }
+  # 
+  # 
+  # smoothed_newusers= var_newusers$smoothed
+  # names(smoothed_newusers)=c("1","2","3","4")
+  # D=double()
+  # for (i in 1:nrow(smoothed_newusers)) {
+  #   D[i]=as.double(names(smoothed_newusers)[which(t(smoothed_newusers[i,])==max(t(smoothed_newusers[i,])))])
+  # }
+  # 
+  # 
+  # smoothed_bounces= var_bounces$smoothed
+  # names(smoothed_bounces)=c("1","2","3", "4")
+  # E=double()
+  # for (i in 1:nrow(smoothed_bounces)) {
+  #   E[i]=as.double(names(smoothed_bounces)[which(t(smoothed_bounces[i,])==max(t(smoothed_bounces[i,])))])
+  # }
+  # 
+  # 
+  # df_retornos$smoothed=as.double(E)
+  # df$smoothed=as.double(c(2,E))
+  # ggplot(df_retornos, aes(x=date, y=sessions, group=1, colour= factor(round(smoothed,0))))+geom_line(size=0.2) + geom_smooth()+theme(axis.text.x = element_text(angle=30))+
+  #   scale_x_discrete(limits=df$date,breaks=df$date[seq(1,366,10)])+scale_colour_discrete(h=c(0,180), l=60, c=180)
+  # 
+  # 
+  # df_retornos$smoothed=as.double(A)
+  # df$smoothed=as.double(c(2,A))
+  # p=ggplot(df, aes(x=date, y=sessions, group=1, colour= factor(smoothed)))+geom_line(size=0.2) + geom_smooth()+theme(axis.text.x = element_text(angle=30))+
+  #   scale_x_discrete(limits=df$date,breaks=df$date[seq(1,400,10)])+scale_colour_manual(labels=c("1","2","3"), values=c("green", "yellow", "red"))
+  # 
+  # 
+  # 
+  # df_retornos$smoothed=as.double(B)
+  # df$smoothed=as.double(c(2,B))
+  # p=ggplot(df_retornos, aes(x=date, y=sessions, group=1, colour= factor(smoothed)))+geom_point(size=3)+geom_line(color="black") + geom_smooth()+theme(axis.text.x = element_text(angle=30))+
+  #   scale_x_discrete(limits=df$date,breaks=df$date[seq(1,366,10)])+scale_colour_discrete(h=c(0,180), l=50)
+  # q=ggplot(df[-1,], aes(x=date, y=sessions, group=1, colour= factor(smoothed)))+geom_point(size=3)+geom_line(color="black") + geom_smooth()+theme(axis.text.x = element_text(angle=30))+
+  #   scale_x_discrete(limits=df$date,breaks=df$date[seq(1,366,10)])+scale_colour_discrete(h=c(0,180), l=50)
+  # 
+  # multiplot(p,q)
+  # 
+  # 
+  # ggplot(data=df_retornos, aes(x=sessions))+
+  #   geom_density(alpha=.2, fill="blue")+
+  #   stat_function(fun=dnorm, args = list(mean=var_sess$mu[1,], sd=sqrt(var_sess$sigma[1,])), colour="green")+
+  #   stat_function(fun=dnorm, args = list(mean=var_sess$mu[2,], sd=sqrt(var_sess$sigma[2,])), colour="red")+
+  #   stat_function(fun=dnorm, args = list(mean=var_sess$mu[4,], sd=sqrt(var_sess$sigma[3,])), colour="orange")+
+  #   stat_function(fun=dnorm, args = list(mean=var_sess$mu[3,], sd=sqrt(var_sess$sigma[4,])), colour="yellow")+
+  #   scale_x_continuous(breaks = round(seq(min(df_retornos$sessions), max(df_retornos$sessions), by = 0.1),1)) 
+  # geom_density(aes(x=users), fill="green", alpha=0.2)+
+  #   geom_density(aes(x=bounces), fill="red", alpha=.2)+
+  #   geom_density(aes(x=newusers), fill="yellow",alpha=0.2)+  
   
   
